f1aacce8668427954898ef52676b5ffce3a3e9c7
Sachin <sachin2013086@akgec.ac.in>
2023-12-17 12:11:06 +0530

    basic implementation

---
+++ lib/data/expense_data.dart
@@ -0,0 +1,85 @@
+import 'package:expense_tracker/dateTime/date_time_helper.dart';
+import 'package:expense_tracker/models/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseData extends ChangeNotifier {
+  // list of all expenses
+  List<ExpenseItem> overallExpenseList = [];
+  // get expense list
+  List<ExpenseItem> getAllExpenseList() {
+    return overallExpenseList;
+  }
+
+  //add new expense
+  void addNewExpense(ExpenseItem newExpense) {
+    overallExpenseList.add(newExpense);
+
+    notifyListeners();
+  }
+
+  // delete expense
+  void deleteExpense(ExpenseItem expense) {
+    overallExpenseList.remove(expense);
+
+    notifyListeners();
+  }
+
+  // get weekday form a dateTime object
+  String getDayName(DateTime dateTime) {
+    switch (dateTime.weekday) {
+      case 1:
+        return "Monday";
+      case 2:
+        return "Tuesday";
+      case 3:
+        return "wednesday";
+      case 4:
+        return "Thursday";
+      case 5:
+        return "Friday";
+      case 6:
+        return "Saturday";
+      case 7:
+        return "Sunday";
+      default:
+        return ' ';
+    }
+  }
+
+//get the date for the start of the week (sunday)
+  DateTime startOfWeekDate() {
+    DateTime? startOfWeek;
+
+    //get todays date
+    DateTime today = DateTime.now();
+
+    //go backwards form today to find sunday
+
+    for (int i = 0; i < 7; i++) {
+      if (getDayName(today.subtract(Duration(days: i))) == 'Sunday') {
+        startOfWeek = today.subtract(Duration(days: i));
+      }
+    }
+    return startOfWeek!;
+  }
+
+// convert overall list of expenses into a daily expense
+
+  Map<String, double> calculateDailyExpenseSummary() {
+    Map<String, double> dailyExpenseSummary = {};
+
+    for (var expense in overallExpenseList) {
+      String date = convertDateTimeToString(expense.dateTime as DateTime);
+      double amount = double.parse(expense.amount);
+
+      if (dailyExpenseSummary.containsKey(date)) {
+        double currentAmount = dailyExpenseSummary[date]!;
+        currentAmount += amount;
+        dailyExpenseSummary[date] = currentAmount;
+      } else {
+        dailyExpenseSummary.addAll({date: amount});
+      }
+    }
+    return dailyExpenseSummary;
+  }
+}
